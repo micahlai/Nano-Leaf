d66a9a430749ae7f5bf19665ad444de34af29cac
Anonymous Shrimp <60944195+Anonymous-Shrimp@users.noreply.github.com>
2020-06-20 18:09:34 -0700

    Code
    ...

---
+++ Final.ino
@@ -0,0 +1,147 @@
+
+
+//By Anonymous Shrimp
+//https://youtube.com/channel/UCs2Sz1gPlWAdET5qcLcZCJw
+//https://github.com/Anonymous-Shrimp
+
+
+#include <ColorConverter.h>
+#include <FastLED.h>
+
+#define LED_PIN     7
+#define NUM_LEDS    100
+#define BRIGHTNESS  50
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
+ColorConverter convert;
+
+
+int irSat1 = 100;
+int ir1 = 3;
+
+int irSat2 = 100;
+int ir2 = 4;
+
+int irSat3 = 100;
+int ir3 = 5;
+
+int irSat4 = 100;
+int ir4 = 6;
+
+int irSat5 = 100;
+int ir5 = 8;
+
+#define UPDATES_PER_SECOND 100
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+
+void setup() {
+  delay( 500 );
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+
+  currentPalette = OceanColors_p;
+  currentBlending = LINEARBLEND;
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+}
+
+
+void loop()
+{
+  ChangePalettePeriodically();
+
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* motion speed */
+
+  FillLEDsFromPaletteColors( startIndex);
+
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  uint8_t brightness = 255;
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+  }
+}
+
+
+//Palette Options: RainbowColors_p, RainbowStripeColors_p,OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+
+void ChangePalettePeriodically()
+{
+  uint8_t secondHand = (millis() / 1000) % 60;
+  static uint8_t lastSecond = 99;
+
+  if ( lastSecond != secondHand) {
+    lastSecond = secondHand;
+    if ( secondHand ==  0)  {
+      currentPalette = PartyColors_p;
+      currentBlending = LINEARBLEND;
+    }
+
+  }
+}
+void SetupTotallyRandomPalette()
+{
+  for ( int i = 0; i < 16; i++) {
+    currentPalette[i] = CHSV( random8(), 255, random8());
+  }
+}
+
+void SetupBlackAndWhiteStripedPalette()
+{
+  fill_solid( currentPalette, 16, CRGB::Black);
+  currentPalette[0] = CRGB::White;
+  currentPalette[4] = CRGB::White;
+  currentPalette[8] = CRGB::White;
+  currentPalette[12] = CRGB::White;
+
+}
+
+void SetupPurpleAndGreenPalette()
+{
+  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+  CRGB green  = CHSV( HUE_GREEN, 255, 255);
+  CRGB black  = CRGB::Black;
+
+  currentPalette = CRGBPalette16(
+                     green,  green,  black,  black,
+                     purple, purple, black,  black,
+                     green,  green,  black,  black,
+                     purple, purple, black,  black );
+}
+
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Black,
+
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Black,
+
+  CRGB::Red,
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Blue,
+  CRGB::Black,
+  CRGB::Black
+};
